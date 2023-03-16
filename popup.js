2fd1310f20f2e060f2b7d9ad456bf560433309af
Afifa <78159461+afifa1819@users.noreply.github.com>
2023-03-16 22:54:36 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,3 +3,12 @@
     { url: "https://www.github.com", importance:2},
     { url: "https://www.youtube.com", importance:1}
   ];
+ 
+chrome.storage.local.set({bookmarks}, function(){
+console.log("Bookmarks stored.");
+});
+
+chrome.storage.local.get("bookmarks" , function(data){
+  const bookmarks = data.bookmarks || [];
+  console.log("Retrieved bookmarks:" , bookmarks);
+});
