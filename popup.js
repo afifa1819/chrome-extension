9b367f3ce3757f9665c34459c1bad00cda367dc2
Afifa <78159461+afifa1819@users.noreply.github.com>
2023-03-16 22:48:13 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,5 @@
+  const bookmarks=[
+    { url: "https://www.google.com", importance:3},
+    { url: "https://www.github.com", importance:2},
+    { url: "https://www.youtube.com", importance:1}
+  ];
